fabae9ecd5fedd6377aba72ffac882d006b5f265
yelsharawy <yelsharawy20@stuy.edu>
2020-11-22 21:21:11 -0500

    Implemented RationalNumber.equals method (doesn't work without reduce implemented yet)

--- RationalNumber.java
+++ RationalNumber.java
@@ -49,7 +49,8 @@ public class RationalNumber extends RealNumber  {
     *@return true when the RationalNumbers have the same numerators and denominators, false otherwise.
     */
     public boolean equals(RationalNumber other) {
-        return false;
+        return getNumerator() == other.getNumerator() &&
+                getDenominator() == other.getDenominator();
     }
 
 

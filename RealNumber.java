c0d68d639466db52d24789c90c50ba9d126f8933
yelsharawy <yelsharawy20@stuy.edu>
2020-11-22 20:34:32 -0500

    Fixed formatting

--- RealNumber.java
+++ RealNumber.java
@@ -1,50 +1,52 @@
-public class RealNumber{
-  private double value;
+public class RealNumber {
 
-  public RealNumber(double v){
-    value = v;
-  }
+    private double value;
 
-  /*
-  *Return the sum of this and the other
-  */
-  public RealNumber add(RealNumber other){
-     //other can be ANY RealNumber, including a RationalNumber
-     //or other subclasses of RealNumber (that aren't written yet)
-     return null;
-  }
+    public RealNumber(double v) {
+        value = v;
+    }
 
-  /*
-  *Return the product of this and the other
-  */
-  public RealNumber multiply(RealNumber other){
+    /*
+    *Return the sum of this and the other
+    */
+    public RealNumber add(RealNumber other) {
+        //other can be ANY RealNumber, including a RationalNumber
+        //or other subclasses of RealNumber (that aren't written yet)
         return null;
-  }
+    }
 
-  /*
-  *Return the this divided by the other
-  */
-  public RealNumber divide(RealNumber other){
+    /*
+    *Return the product of this and the other
+    */
+    public RealNumber multiply(RealNumber other) {
         return null;
-  }
+    }
 
-  /*
-  *Return the this minus the other
-  */
-  public RealNumber subtract(RealNumber other){
-    return null;
-  }
+    /*
+    *Return the this divided by the other
+    */
+    public RealNumber divide(RealNumber other) {
+        return null;
+    }
+
+    /*
+    *Return the this minus the other
+    */
+    public RealNumber subtract(RealNumber other) {
+        return null;
+    }
 
 
-  public double getValue(){
-    return value;
-  }
+    public double getValue() {
+        return value;
+    }
 
-  public boolean equals(RealNumber other){
-     return true;
-   }
+    public boolean equals(RealNumber other) {
+        return true;
+    }
 
-  public String toString(){
-    return ""+value;
-  }
+    public String toString() {
+        return ""+value;
+    }
+
 }

d22c8329a7190c5458a72f1b9beacf4f34da0f1e
yelsharawy <yelsharawy20@stuy.edu>
2020-11-24 20:51:52 -0500

    Fixed Number doc comment formatting

--- Number.java
+++ Number.java
@@ -2,15 +2,16 @@ public abstract class Number {
 
     public abstract double getValue();
 
-    /*return 0 when this Number equals the other RealNumber
-    return a negative value when this Number is smaller than the other Number
-    return a positive value when this Number is larger than the other Number
+    /**
+    *return 0 when this Number equals the other RealNumber
+    *return a negative value when this Number is smaller than the other Number
+    *return a positive value when this Number is larger than the other Number
     */
     public int compareTo(Number other) {
         return equals(other) ? 0 : Double.compare(getValue(), other.getValue());
     }
 
-    /*
+    /**
     *Return true when the % difference of the values
     *are within 0.00001 of eachother.
     *Special case: if one is exactly zero, the other must be exactly zero.
